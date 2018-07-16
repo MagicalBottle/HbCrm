0711ef3663b5f755868226fcce4e543dbba06a7a
MagicalBottle <32764458+MagicalBottle@users.noreply.github.com>
2018-07-16 16:13:03 +0800

    init

---
+++ src/Tests/HbCrm.Core.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HbCrm.Core.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
