bd8d7559108ee6444dc4eebb496ba33242083d1d
MagicalBottle <32764458+MagicalBottle@users.noreply.github.com>
2018-08-03 17:20:54 +0800

    test

---
+++ src/Tests/HbCrm.Data.Tests/SchemaTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using HbCrm.Core;
+using HbCrm.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HbCrm.Data.Tests
+{
+    public class SchemaTests
+    {
+        [Fact]
+        public void Can_Generate_Schema()
+        {
+            DbContextOptions<HbCrmObjectContext> options = new DbContextOptions<HbCrmObjectContext>();
+
+            HbCrmObjectContext context = new HbCrmObjectContext(options);
+            string script=context.GenerateCreateScript();
+
+        }
+    }
+}
