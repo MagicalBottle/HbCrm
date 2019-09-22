64500bc7ed5f689fd09e1467f24f387388ff1276
mochangping <41366696+mochangping@users.noreply.github.com>
2019-09-22 14:55:07 +0800

    测试提交

--- src/Tests/HbCrm.Core.Tests/Program.cs
+++ src/Tests/HbCrm.Core.Tests/Program.cs
@@ -6,7 +6,7 @@ namespace HbCrm.Core.Tests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
